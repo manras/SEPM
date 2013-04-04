65367a5bb65a623a86cdcb592de4b0d8535309c6
Christoph Klotz <klotzchristoph@gmail.com>
2013-04-04 17:56:47 +0200

    added important code

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,10 @@
 public class Test {
 	public static void main(String... args) {
 		System.out.println("Test");
+		System.out.println("Beim Klotz gehts jetzt auch wieder...");// most
+																	// important
+																	// code in
+																	// whole
+																	// code
 	}
 }
