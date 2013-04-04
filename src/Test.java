16e2e067fba76535cb65997026f64cc7587f3336
Christoph Klotz <klotzchristoph@gmail.com>
2013-04-04 16:11:14 +0200

    blabla

--- src/Test.java
+++ src/Test.java
@@ -5,5 +5,6 @@ public class Test {
 				.println("Ich bin die Veränderung, die sich verändert hat und verdammt nochmal ńirgendwo funktioniert!!");
 		System.out.println("Schreibts doch mal was gscheites es Stoffeln es!");
 		System.out.println("Chris war hier");
+		System.out.println("WilhelmScream");
 	}
 }
