12d03099758bba19559f783b9b1b81411cc0d0c5
SEPM <christoph.bessei@student.uibk.ac.at>
2013-04-04 16:52:49 +0300

    Update Test.java
    lala

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,6 @@
-
 public class Test {
 	public static void main(String...args) {
 		System.out.println("Test");
-		System.out.println("Ich bin die Veränderung, die sich verändert hat");
+		System.out.println("Ich bin die Veränderung, die sich verändert hat und verdammt nochmal ńirgendwo funktioniert!!");
 	}
 }
