69100298321b0f11fa92fda01dcd1adc302e61cf
mobru <d.mobru@gmail.com>
2013-04-28 17:34:22 +0200

    Trying to get the model package to show in github.

---
+++ src/Model/Comment.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.util.GregorianCalendar;
+
+/**
+ * 
+ * @author Daniel Moosbrugger
+ * 
+ */
+public class Comment {
+
+	private User author;
+	private GregorianCalendar date;
+
+	public Comment() {
+
+	}
+
+}
